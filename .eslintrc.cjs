104230376f5fb055ae77fdf1b435206b96676d25
naoki-yoshii <edoaruai@gmail.com>
2025-11-12 16:07:29 +0900

    test: push guard check

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+﻿/** Monorepo root ESLint */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  env: { node: true, es2022: true, browser: true },
+  ignorePatterns: ["**/node_modules/**", "**/dist/**", "**/.next/**"]
+};
